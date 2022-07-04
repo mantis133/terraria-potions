94efdaec83e723e2462dfb45eb2350348e178e22
mantis133 <mantisshrimp40@gmail.com>
2022-07-04 21:38:54 +1200

    listing and hover effects

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const recipeJson = "recipes.json";
 
 let allPotionsList = document.getElementById("list-of-potions");
 let allPotionsUnorderedList = document.createElement("ul");
+allPotionsUnorderedList.setAttribute("id", "all-potions-list");
 allPotionsList.appendChild(allPotionsUnorderedList);
 let selectedPotionsList = document.getElementById("list-of-selected-potions");
 let potionsImg = document.getElementById("potions-icon");
@@ -10,65 +11,92 @@ let potionsDescription = document.getElementById("potions-information");
 let infoButton = document.getElementById("info-btn");
 let IngredientsButton = document.getElementById("ingredients-btn");
 
-
-
-/* 
-<li class="potions-container-list-item">
-    <div class="potion-container">
-        <label for="potion-0">
-            <input type="checkbox" name="select-0" id="potion-0">
-            <img id="potion-0-img" src="" alt="">
-            <p id="potion-0-name"></p>
-            <p id="potion-0-description"></p>
-        </label>
-    </div>
-</li> 
-*/
-
 $.getJSON( recipeJson, function( data ) {
     let potions = data.potions;
     for (let i = 0; i < potions.length; i++) {
         let potion = potions[i];
-        
+        // collect all the potion information from the json file
         let potionName = potion.name;
         let potionIngredients = potion.ingredients;
         let potionDescription = potion.description;
         let potionDuration = potion.duration;
         let potionImg = potion.icon;
         let potionHardmode = potion.hardmode;
-
+        // create a list item for each potion
         let potionsConatainerListItem = document.createElement("li");
         potionsConatainerListItem.classList.add("potions-container-list-item");
-
+        // create the potion container to place the potion information into
         let potionContainer = document.createElement("div");
         potionContainer.classList.add("potion-container");
+        potionContainer.onmouseover = function() {
+            
+            document.getElementById("potion-image").removeChild(document.getElementById("potion-image").firstChild);
+            document.getElementById("potion-name").removeChild(document.getElementById("potion-name").firstChild);
+            document.getElementById("potion-duration").removeChild(document.getElementById("potion-duration").firstChild);
+            document.getElementById("potion-description").removeChild(document.getElementById("potion-description").firstChild);
+            
+            let hoveredPotionIcon = document.createElement("img");
+            hoveredPotionIcon.setAttribute("src", potionImg);
+            document.getElementById("potion-image").appendChild(hoveredPotionIcon);
 
+            let hoveredPotionName = document.createElement("h3");
+            hoveredPotionName.innerHTML = potionName;
+            hoveredPotionName.style.fontFamily = "andyBold";
+            document.getElementById("potion-name").appendChild(hoveredPotionName);
+
+            let hoveredPotionDuration = document.createElement("p");
+            hoveredPotionDuration.innerHTML = potionDuration;
+            hoveredPotionDuration.style.fontFamily = "andyBold";
+            document.getElementById("potion-duration").appendChild(hoveredPotionDuration);
+
+            let hoveredPotionDescription = document.createElement("p");
+            hoveredPotionDescription.innerHTML = potionDescription;
+            hoveredPotionDescription.style.fontFamily = "andyBold";
+            document.getElementById("potion-description").appendChild(hoveredPotionDescription);
+        }
+        potionContainer.style.width = "200px";
+        potionContainer.style.height = "80px";
+        // create the label for the potion
         let potionLabel = document.createElement("label");
         potionLabel.setAttribute("for", "potion-" + i);
-
+        // create the checkbox to move the potion to the selected potions list
         let potionInput = document.createElement("input");
         potionInput.setAttribute("type", "checkbox");
         potionInput.setAttribute("name", "select-" + i);
         potionInput.setAttribute("id", "potion-" + i);
-
+        potionInput.style.float = "left";
+        // create the potion icon
         let potionImgElement = document.createElement("img");
         potionImgElement.setAttribute("id", "potion-" + i + "-img");
         potionImgElement.setAttribute("src", potionImg);
         potionImgElement.setAttribute("alt", potionName);
-
+        potionImgElement.style.float = "left";
+        // create the potion name
         let potionNameElement = document.createElement("p");
         potionNameElement.setAttribute("id", "potion-" + i + "-name");
         potionNameElement.innerHTML = potionName;
-
+        potionNameElement.style.fontFamily = "andyBold";
+        potionNameElement.style.float = "left";
+        potionNameElement.style.margin = "0px";
+        potionNameElement.style.position = "relative";
+        potionNameElement.style.left = "5px";
+        // create the potion description
         let potionDescriptionElement = document.createElement("p");
         potionDescriptionElement.setAttribute("id", "potion-" + i + "-description");
         potionDescriptionElement.innerHTML = potionDescription;
-
+        potionDescriptionElement.style.fontFamily = "andyBold";
+        potionDescriptionElement.style.width = "160px";
+        potionDescriptionElement.style.position = "relative";
+        potionDescriptionElement.style.margin = "0px";
+        potionDescriptionElement.style.top = "0px";
+        potionDescriptionElement.style.right = "5px";
+        potionDescriptionElement.style.float = "right";
+        // append the potion information to the potion container
         potionLabel.appendChild(potionInput);
         potionLabel.appendChild(potionImgElement);
         potionLabel.appendChild(potionNameElement);
         potionLabel.appendChild(potionDescriptionElement);
-
+        // append the potion container to the potion container list item
         potionContainer.appendChild(potionLabel);
         potionsConatainerListItem.appendChild(potionContainer);
         allPotionsUnorderedList.appendChild(potionsConatainerListItem);
@@ -76,59 +104,37 @@ $.getJSON( recipeJson, function( data ) {
         // add event listener to the checkbox
         let potionInputElement = document.getElementById("potion-" + i);
         potionInputElement.addEventListener("change", function() {
-            // if (potionInputElement.checked) {
-            //     // add potion to the selected potions list
-            //     let potionContainer = document.createElement("div");
-            //     potionContainer.classList.add("potion-container");
-            //     potionContainer.setAttribute("id", "potion-" + i);
-
-            //     let potionImgElement = document.createElement("img");
-            //     potionImgElement.setAttribute("src", potionImg);
-            //     potionImgElement.setAttribute("alt", potionName);
-
-            //     let potionNameElement = document.createElement("p");
-            //     potionNameElement.innerHTML = potionName;
-
-            //     let potionDescriptionElement = document.createElement("p");
-            //     potionDescriptionElement.innerHTML = potionDescription;
-
-            //     potionContainer.appendChild(potionImgElement);
-            //     potionContainer.appendChild(potionNameElement);
-            //     potionContainer.appendChild(potionDescriptionElement);
-
-            //     selectedPotionsList.appendChild(potionContainer);
-            //     potionsImg.setAttribute("src", potionImg);
-            //     potionsDescription.innerHTML = potionDescription;
-            // } else {
-            //     // remove potion from the selected potions list
-            //     let selectedPotionContainer = document.getElementById("potion-" + i);
-            //     selectedPotionsList.removeChild(selectedPotionContainer);
-            //     if (selectedPotionsList.children.length === 0) {
-            //         potionsImg.setAttribute("src", "");
-            //         potionsDescription.innerHTML = "";
-            //     }
-            // }
             if (potionInputElement.checked) {
-                let potname = document.createElement("p")
-                potname.innerHTML = potionName;
-                potname.setAttribute("id", "potion-" + i + "-namee");
-                selectedPotionsList.appendChild(potname);
+                // create a conatiner to put selected potion information into
+                let selectedPotionContainer = document.createElement("div");
+                selectedPotionContainer.classList.add("selected-potion-container");
+                selectedPotionContainer.setAttribute("id", "potion-" + i + "-container-selected");
+                selectedPotionsList.appendChild(selectedPotionContainer);
+                // create the potion icon to put into the container
+                let potionImgElement = document.createElement("img");
+                potionImgElement.setAttribute("src", potionImg);
+                potionImgElement.setAttribute("alt", potionName);
+                potionImgElement.setAttribute("id", "potion-" + i + "-img-selected");
+                potionImgElement.classList.add("potion-img-selected");
+                // create the potion name to put into the container
+                let potionNameElement = document.createElement("p");
+                potionNameElement.innerHTML = potionName;
+                potionNameElement.setAttribute("id", "potion-" + i + "-name-selected");
+                potionNameElement.classList.add("potion-name-selected");
+                // create the potion description to put into the container
+                let potionDescriptionElement = document.createElement("p");
+                potionDescriptionElement.innerHTML = potionDescription;
+                potionDescriptionElement.setAttribute("id", "potion-" + i + "-description-selected");
+                potionDescriptionElement.classList.add("potion-description-selected");
+                // place all the potion info into the container
+                selectedPotionContainer.appendChild(potionImgElement);
+                selectedPotionContainer.appendChild(potionNameElement);
+                selectedPotionContainer.appendChild(potionDescriptionElement);
             } else {
-                let pootname = document.getElementById("potion-" + i + "-namee");
-                selectedPotionsList.removeChild(pootname);
+                // remove the potion from the selected potions list
+                let selectedPotionContainer = document.getElementById("potion-" + i + "-container-selected");
+                selectedPotionsList.removeChild(selectedPotionContainer);
             }
         });
-    }
+    } // end of for loop
 });
-
-
-
-// let checkBox = document.getElementById('potion-0'); //testing event listener on checkbox event listener with form
-// checkBox.addEventListener('click', function() {
-//     if (checkBox.checked) {
-//         selectedPotionsList.appendChild(allPotionsList.children[0]);
-//     } else {
-//         allPotionsList.appendChild(selectedPotionsList.children[0]);
-//     }
-// }
-// );
